d5a0791694542bb2b1f0f99a74ca9c3f9658fd4a
Collin Shriner <collinshriner9@gmail.com>
2023-06-19 10:02:29 -0400

    working with spring JDBC

--- src/main/java/shriner/springpractice/recipeproject/controllers/indexController.java
+++ src/main/java/shriner/springpractice/recipeproject/controllers/indexController.java
@@ -2,12 +2,31 @@ package shriner.springpractice.recipeproject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shriner.springpractice.recipeproject.domain.Category;
+import shriner.springpractice.recipeproject.domain.UnitOfMeasure;
+import shriner.springpractice.recipeproject.repositories.CategoryRepository;
+import shriner.springpractice.recipeproject.repositories.UnitOfMeasureRepository;
+
+import java.util.Optional;
 
 @Controller
 public class indexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public indexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndex(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category Id is: " + categoryOptional.get().getId());
+        System.out.println("Unit of Measure Id is: " + unitOfMeasureOptional.get().getId());
 
         return "index";
     }
--- src/main/java/shriner/springpractice/recipeproject/repositories/UnitOfMeasureRepository.java
+++ src/main/java/shriner/springpractice/recipeproject/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package shriner.springpractice.recipeproject.repositories;
 import org.springframework.data.repository.CrudRepository;
 import shriner.springpractice.recipeproject.domain.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
